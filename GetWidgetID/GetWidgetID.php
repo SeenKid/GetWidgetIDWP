143429a4d134a723211f16910b6418b1a6df9bab
Yann@SeenKid <30565751+SeenKid@users.noreply.github.com>
2023-06-16 10:38:27 +0200

    Add files via upload

---
+++ GetWidgetID/GetWidgetID.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+* Plugin Name: GetWidgetID
+* Author: Yann Berlemont - Creative Technologies SA
+* Version: 1.1
+* Description: Montre l'ID des widgets
+* Plugin URI: https://github.com/SeenKid/GetWidgetIDWP
+*/
+
+// GET WIDGET ID
+
+add_action('in_widget_form', 'spice_get_widget_id');
+function spice_get_widget_id($widget_instance) {
+    // Check if the widget is already saved or not.
+    if ($widget_instance->number=="__i__"){
+        echo "<p><strong>Widget ID is</strong>: Pls save the widget first!</p>";
+    } else {
+        echo "<p><strong>Widget ID is: </strong>" .$widget_instance->id. "</p>";
+    }
+}
